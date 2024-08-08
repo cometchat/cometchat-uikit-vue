9a35e78756f21d6fc8222da7003e18707caad070
birendra.gupta <birendraprasad.gupta@cometchat.com>
2024-08-08 22:02:45 +0530

    Update UiKit to v4.3.15

--- dist/src/components/shared/Utils/MessageUtils.d.ts
+++ dist/src/components/shared/Utils/MessageUtils.d.ts
@@ -13,4 +13,10 @@ export declare class MessageUtils {
     getContentView(message: CometChat.BaseMessage, template: CometChatMessageTemplate): any;
     getBubbleWrapper(message: CometChat.BaseMessage, template: CometChatMessageTemplate): any;
     getMessageBubble(baseMessage: CometChat.BaseMessage, template: CometChatMessageTemplate, messageBubbleStyle: any, alignment: MessageBubbleAlignment): any;
+    /**
+ * Function to check if a user's status should be visible
+ * @param {CometChat.User | CometChat.GroupMember} user - The user whose status visibility is to be checked.
+ * @return {boolean} - Returns `true` if the user's status should be hidden (blocked or offline), `false` otherwise.
+ */
+    getUserStatusVisibility(user?: CometChat.User | CometChat.GroupMember): boolean;
 }
